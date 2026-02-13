c4b193b1d23a7f391908503d3954a975e6333aad
Vivek Varanasi <vivek.vardhan@thinkartha.com>
2026-02-13 16:28:38 -0700

    Stop tracking .next and tighten .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/vivekvardhan/code/data_quality_magic/app/layout\":[\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
